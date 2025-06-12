36e3ef558550bc350b991f38212c7a0e31fe9eef
Alex Siritsa <alex.siritsa@bartgroup.co>
2025-06-12 18:24:48 +0300

    Homework 3.1

---
+++ js/types.js
@@ -0,0 +1,19 @@
+"use strict";
+
+const footSize = 27.5;
+const numberOfAntsOnThePlanet = BigInt(20000000000000000000000);
+const string = 'I don\'t like monday';
+const boolean = true;
+const secureFieldName = Symbol('secret_data_field');
+const customerData = {firstname: 'Alex', lastname: 'Siritsya'};
+const infoAboutSecretUser = null;
+const notExistElement = undefined;
+
+console.log(typeof footSize);
+console.log(typeof numberOfAntsOnThePlanet);
+console.log(typeof string);
+console.log(typeof boolean);
+console.log(typeof secureFieldName);
+console.log(typeof customerData);
+console.log(typeof infoAboutSecretUser);
+console.log(typeof notExistElement);
